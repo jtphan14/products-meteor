eab2624211f75f58559a316f57f65ddb53dba22d
Jacqueline <jtphan14@gmail.com>
2016-08-08 20:47:27 -0700

    Add basic css styling

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,8 +4,8 @@ import './body.html';
 
 Template.body.helpers({
   products: [
-    { picture: 'insert pic here', text: 'This is product 1',  description: 'This is sample description', price: '$100'},
-    { picture: 'insert pic here', text: 'This is product 2',  description: 'This is sample description', price: '$100'},
-    { picture: 'insert pic here', text: 'This is product 3',  description: 'This is sample description', price: '$100'},
+    { picture: 'insert pic here', text: 'This is product 1',  description: 'This is sample description for those who need a description', price: '$100'},
+    { picture: 'insert pic here', text: 'This is product 2',  description: 'This is sample description for those who need a description', price: '$100'},
+    { picture: 'insert pic here', text: 'This is product 3',  description: 'This is sample description for those who need a description', price: '$100'},
   ],
 });
