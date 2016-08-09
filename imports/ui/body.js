c6e479a2891ad795746930114f65ceaf92475800
Jacqueline <jtphan14@gmail.com>
2016-08-08 21:13:39 -0700

    Add mongo collection for products

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,11 @@
 import { Template } from 'meteor/templating';
 
+import { Products } from '../api/products.js';
+
 import './body.html';
 
 Template.body.helpers({
-  products: [
-    { picture: 'insert pic here', text: 'This is product 1',  description: 'This is sample description for those who need a description', price: '$100'},
-    { picture: 'insert pic here', text: 'This is product 2',  description: 'This is sample description for those who need a description', price: '$100'},
-    { picture: 'insert pic here', text: 'This is product 3',  description: 'This is sample description for those who need a description', price: '$100'},
-  ],
+  products() {
+    return Products.find({});
+  },
 });
